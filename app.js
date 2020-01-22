3bae4a1c73665bd272e0e531a9227a537aff166a
SergioLopezBailon <47179066+SergioLopezBailon@users.noreply.github.com>
2020-01-22 20:35:12 +0100

    Segundo commit
    He añadido una clase de iconos al proyecto y he avanzado en la clase app

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 window.addEventListener('load',()=>{
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position =>{
             long=position.coords.longitude;
             lat=position.coords.latitude;
-            const api=`https://api.darksky.net/forecast/c934ddd5ad2b595e92eb48b5fe9d6910/${lat},${long}`;
+
+            const proxy='https://cors-anywhere.herokuapp.com/';
+            const api=`${proxy}https://api.darksky.net/forecast/c934ddd5ad2b595e92eb48b5fe9d6910/${lat},${long}`;
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+                .then(data=>{
+                    console.log(data);
+                    const {temperature, summary, icon } = data.currently;
+                    //Sett DOM Elements from the API
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
+
+                })
         });
+    }
+
+    function setIcons(icon, iconID){
+        const skycons = new skycons({color:"white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
     }
 });
